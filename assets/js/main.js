74ef3ab22f35c0bb6870f01ce967c571e23d2646
nextdev2 <nextdev0.02@gmail.com>
2023-09-16 11:04:51 +0600

    update

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+// Client Logo Slider
+
+let clientswiper = new Swiper(".clients-slider", {
+    slidesPerView: 6,
+    spaceBetween: 10,
+    pagination: {
+      clickable: true,
+    },
+  });
+
+
+  // Client Logo Slider
+
+let galleryswiper = new Swiper(".gallery-slider", {
+  slidesPerView: 3,
+  spaceBetween: 1,
+  pagination: {
+    clickable: true,
+  },
+});
+
+
